05087096113f064642b7efabe10baaa2c0f9194b
srinivas <vasumayeee.99@gmail.com>
2023-12-21 12:55:13 +0530

    roboshop-scripts-catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -21,41 +21,35 @@ then
 else
     echo -e "$G You are the root user $N"
 fi 
-dnf module disable nodejs -y
-validate $? "Disable" &>> $logfile
-dnf module enable nodejs:18 -y
-validate $? "Enable" &>> $logfile
-dnf install nodejs -y
-validate $? "Install" &>> $logfile
-useradd roboshop
-validate $? "User Add" &>> $logfile
-mkdir /app
-validate $? "Make dir" &>> $logfile
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
-validate $? "Download zip" &>> $logfile
+dnf module disable nodejs -y &>> $logfile
+validate $? "Disable"
+dnf module enable nodejs:18 -y &>> $logfile
+validate $? "Enable" 
+dnf install nodejs -y &>> $logfile
+validate $? "Install"
+useradd roboshop &>> $logfile
+validate $? "User Add"
+mkdir /app 
+validate $? "Make dir"
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $logfile
+validate $? "Download zip" 
 cd /app
-unzip /tmp/catalogue.zip
-validate $? "unzip download" &>> $logfile
-npm install
-validate $? "Download dependencies" &>> $logfile
+unzip /tmp/catalogue.zip &>> $logfile
+validate $? "unzip download"
+npm install &>> $logfile
+validate $? "Download dependencies"
 #copy from the obsolute path 
-cp /home/centos/robo-shell/catalogue.service /etc/systemd/system/catalogue.service
-validate $? "copy from obs path" &>> $logfile
-systemctl daemon-reload
-validate $? "catalogue DRL" &>> $logfile
-systemctl enable catalogue
-validate $? "Enable Cat" &>> $logfile
-systemctl start catalogue
-validate $? "Start catalogue" &>> $logfile
-validate $? "Start" &>> $logfile
-cp /home/centos/robo-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-validate $? "copy mongodb repo" &>> $logfile
-[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-gpgcheck=0
-enabled=1
-dnf install mongodb-org-shell -y
-validate $? "Installing mongodb client" &>> $logfile
-mongo --host mongodb.sridevops.online </app/schema/catalogue.js
-validate $? "Loading catalogue data into mongoDB" &>> $logfile
+cp /home/centos/robo-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $logfile
+validate $? "copy from obs path"
+systemctl daemon-reload &>> $logfile
+validate $? "catalogue DRL"
+systemctl enable catalogue &>> $logfile
+validate $? "Enable Cat"
+systemctl start catalogue &>> $logfile
+validate $? "Start catalogue"
+cp /home/centos/robo-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $logfile
+validate $? "copy mongodb repo"
+dnf install mongodb-org-shell -y &>> $logfile
+validate $? "Installing mongodb client"
+mongo --host mongodb.sridevops.online </app/schema/catalogue.js &>> $logfile
+validate $? "Loading catalogue data into mongoDB"
