bd663b0e0be6f8b174fc28562ef702c882e4ddb6
srinivas <vasumayeee.99@gmail.com>
2023-12-23 13:55:24 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+timestamp=$(date +%F-%H-%M-%S)
+logfile="/tmp/$0-$timestamp.log"
+exec &>$logfile
+echo "Script started executing at $timestamp" &>> $logfile
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error: Please run the script with root user $N"
+else
+    echo -e "$G You are the root user $N"
+fi 
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+validate $? "Installing remi release"
+dnf module enable redis:remi-6.2 -y
+validate $? "Enabling redis"
+dnf install redis -y
+validate $? "Installing Redis"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf
+validate $? " Allowing remote connections"
+
+systemctl enable redis
+validate $? "Enable redis"
+
+systemctl start redis
+validate $? "Start redis"
---
+++ user.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+timestamp=$(date +%F-%H-%M-%S)
+logfile="/tmp/$0-$timestamp.log"
+
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error: Please run the script with root user $N"
+else
+    echo -e "$G You are the root user $N"
+fi 
+
+dnf module disable nodejs -y &>> $logfile
+validate $? "Disable"
+dnf module enable nodejs:18 -y &>> $logfile
+validate $? "Enable" 
+dnf install nodejs -y &>> $logfile
+validate $? "Install"
+
+id roboshop
+
+if [ if $? -ne 0 ]
+then 
+    useradd roboshop
+    validate $? "roboshop user add"
+else
+    echo -e "roboshop user already exists $Y skipping $N"
+fi
+
+mkdir -p /app 
+validate $? "Make dir"
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $logfile
+validate $? "Download zip" 
+cd /app
+unzip -o /tmp/catalogue.zip &>> $logfile
+validate $? "unzip download"
+npm install &>> $logfile
+validate $? "Download dependencies"
+cp /home/centos/robo-shell/user.service /etc/systemd/system/user.service
+validate $? "copy from obs path"
+systemctl daemon-reload &>> $logfile
+validate $? "user DRL"
+systemctl enable user &>> $logfile
+validate $? "Enable User"
+systemctl start user &>> $logfile
+validate $? "Start User"
+cp /home/centos/robo-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "copy mongodb repo"
+dnf install mongodb-org-shell -y &>> $logfile
+validate $? "Installing mongodb client"
+mongo --host mongodb.sridevops.online </app/schema/user.js &>> $logfile
+validate $? "Loading user data into mongoDB"
