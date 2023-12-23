41033f76aa2a72d0032d66ddf1b21f2dbd0d3637
srinivas <vasumayeee.99@gmail.com>
2023-12-23 14:14:33 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -33,7 +33,7 @@ validate $? "Enabling redis"
 dnf install redis -y
 validate $? "Installing Redis"
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf
 validate $? " Allowing remote connections"
 
 systemctl enable redis
