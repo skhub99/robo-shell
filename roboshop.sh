08d98c36e70cb69b3fca2ff6a5ea12fbfa91b8b8
srinivas <vasumayeee.99@gmail.com>
2024-01-04 14:45:20 +0530

    ec2-iam-role

--- roboshop.sh
+++ roboshop.sh
@@ -6,9 +6,7 @@ SG_ID=sg-073d40c36b7ddfbf2
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch")
 
 for i in "$(instances[@])"
-
 do
-
     echo "instance is: $i"
     if [ $i == "mongodb" ] || if [ $i == "mysql" ] || if [ $i == "shipping" ]
     then
@@ -17,5 +15,4 @@ do
         instance_type="t2.micro"
     fi
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_type --security-group-ids sg-073d40c36b7ddfbf2
-
 done
