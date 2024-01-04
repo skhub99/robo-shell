3e79842f8165ffc40cf6abfae51db040acea2d6f
srinivas <vasumayeee.99@gmail.com>
2024-01-04 16:39:47 +0530

    ec2-iam-role

--- roboshop.sh
+++ roboshop.sh
@@ -13,5 +13,5 @@ do
     else
         instance_type="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_type --security-group-ids sg-073d40c36b7ddfbf2
+    "aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_type --security-group-ids sg-073d40c36b7ddfbf2"
 done
