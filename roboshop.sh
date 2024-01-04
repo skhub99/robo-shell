bd531287206a8e1d14172853ba7303f5b1b17031
srinivas <vasumayeee.99@gmail.com>
2024-01-04 15:42:38 +0530

    ec2-iam-role

--- roboshop.sh
+++ roboshop.sh
@@ -3,9 +3,9 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-073d40c36b7ddfbf2
 
-instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch")
+instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
-for i in "$(instances[@])"
+for i in "${instances[@]}"
 do
     echo "instance is: $i"
     if [ $i == "mongodb" ] || if [ $i == "mysql" ] || if [ $i == "shipping" ]
