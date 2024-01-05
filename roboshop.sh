caf1712e0d452ced2c0c46bf6933a5165f9ed583
srinivas <vasumayeee.99@gmail.com>
2024-01-05 13:03:40 +0530

    aws instances

--- roboshop.sh
+++ roboshop.sh
@@ -13,5 +13,6 @@ do
     else
         instance_type="t2.micro"
     fi
-    aws ec2 run-instances --image-id $AMI --instance-type $instance_type --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_Address=$(aws ec2 run-instances --image-id $AMI --instance-type $instance_type --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --Query 'Instances[0].PrivateIpAddress' --output text)
+    echo " $i = $IP_Address"
 done
